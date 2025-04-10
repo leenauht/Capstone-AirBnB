3d91cfe69d4f150c26b9d6216939cecad8fad832
ThanhVinh189 <tham13734@gmail.com>
2025-04-10 11:57:53 +0700

    Upadate file Auth

--- src/pages/AdminTemplate/AuthPage/index.jsx
+++ src/pages/AdminTemplate/AuthPage/index.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from "react";
-import { actLogin, logout } from "./slice";
+import { useRef } from "react";
+import { actLogin } from "./slice";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import { Form, Input, Button, Typography } from "antd";
@@ -15,32 +15,14 @@ export default function AuthPage() {
 
   const handleLogin = async (values) => {
     try {
-      // Gọi dispatch để thực hiện đăng nhập và unwrap kết quả
-      const result = await dispatch(actLogin(values)).unwrap();
-      console.log("Kết quả đăng nhập:", result.userInfo?.role); // In ra kết quả
-
-      // Kiểm tra role của người dùng (bất kể viết hoa hay viết thường)
-      const role = result.userInfo?.role;
-      if (role && role.toUpperCase() === "USER") {
-        toast.error("Bạn không có quyền đăng nhập!", {
-          position: "bottom-right",
-          autoClose: 2000,
-          theme: "colored",
-        });
-
-        errorToastShown.current = true;
-        dispatch(logout());
-        return;
-      }
-
-      // Nếu role hợp lệ, thì hiển thị thông báo đăng nhập thành công
+      await dispatch(actLogin(values)).unwrap();
+      errorToastShown.current = false;
       toast.success("Đăng nhập thành công!", {
         position: "bottom-right",
         autoClose: 1000,
         theme: "colored",
       });
 
-      // Chuyển hướng sau 1 giây
       setTimeout(() => {
         navigate("/admin/QuanLyNguoiDung/1", { replace: true });
       }, 1000);
@@ -50,19 +32,8 @@ export default function AuthPage() {
         autoClose: 2000,
         theme: "colored",
       });
-      errorToastShown.current = true;
     }
   };
-
-  useEffect(() => {
-    if (state.error && !errorToastShown.current) {
-      toast.error(state.error, {
-        position: "bottom-right",
-        autoClose: 2000,
-        theme: "colored",
-      });
-    }
-  }, [state.error, state.data]);
 
   // Nếu đã đăng nhập rồi, chuyển hướng đến trang quản lý người dùng
   if (state.data && state.data.userInfo?.role.toUpperCase() !== "USER") {
