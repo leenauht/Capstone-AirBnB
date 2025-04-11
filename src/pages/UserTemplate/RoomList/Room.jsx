c06a36aca8ff06bf788391b65440740b1fc3db31
thuan_lee <lnauht0406@gmail.com>
2025-04-11 19:52:08 +0700

    update like room

--- src/pages/UserTemplate/RoomList/Room.jsx
+++ src/pages/UserTemplate/RoomList/Room.jsx
@@ -2,16 +2,19 @@ import { Card } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Heart from "../../../Icons/Heart";
+import { useSelector } from "react-redux";
 
 export default function Room({ location, room }) {
+  const { userInfo } = useSelector((state) => state.userInfoReducer);
   const imgDefault =
     "https://a0.muscache.com/im/pictures/miso/Hosting-1296043685685721430/original/4d135661-995c-4a54-a0bc-d714c3089934.jpeg?im_w=720";
   const [isLike, setIsLike] = useState(false);
   const navigate = useNavigate();
+  const cacheName = `${userInfo?.id || "anonymus"}-roomLike`;
 
   const onLike = (status) => {
     let newRoomLike = [];
-    const roomLike = localStorage.getItem("roomLike");
+    const roomLike = localStorage.getItem(cacheName);
     if (roomLike) {
       const dataJSON = JSON.parse(roomLike);
       if (status) {
@@ -22,18 +25,17 @@ export default function Room({ location, room }) {
     } else {
       newRoomLike = [room.id];
     }
-    localStorage.setItem("roomLike", JSON.stringify(newRoomLike));
+
+    localStorage.setItem(cacheName, JSON.stringify(newRoomLike));
     setIsLike(status);
   };
 
   useEffect(() => {
-    const roomLike = localStorage.getItem("roomLike");
-    if (roomLike) {
-      const dataJSON = JSON.parse(roomLike);
-      const statusRoom = dataJSON.find((item) => room.id === item);
-      if (statusRoom) setIsLike(true);
-    }
-  }, []);
+    const roomLike = localStorage.getItem(cacheName);
+    const dataJSON = JSON.parse(roomLike) || [];
+    const statusRoom = dataJSON.find((item) => room.id === item);
+    setIsLike(!!statusRoom);
+  }, [cacheName]);
 
   return (
     <Card
